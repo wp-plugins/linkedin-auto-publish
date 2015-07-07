52c357830056fb3b40ef42ff0df5aaf05ccf6362
f1logic <f1logic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 15:13:23 +0000

    linkedin-auto-publish  1.3 released
    git-svn-id: https://plugins.svn.wordpress.org/linkedin-auto-publish/tags/1.3@1194179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/authorization.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+
+$state=md5(get_home_url());
+
+$redirecturl=urlencode(admin_url('admin.php?page=linkedin-auto-publish-settings'));
+
+$lnappikey=get_option('xyz_lnap_lnapikey');
+$lnapisecret=get_option('xyz_lnap_lnapisecret');
+
+if(isset($_POST['lnauth']))
+{
+	if(!isset($_GET['code']))
+	{
+		$linkedin_auth_url='https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id='.$lnappikey.'&redirect_uri='.$redirecturl.'&state='.$state.'&scope=w_share+rw_company_admin';//rw_groups not included as it requires linkedin partnership agreement
+		wp_redirect($linkedin_auth_url);
+		echo '<script>document.location.href="'.$linkedin_auth_url.'"</script>';
+		die;
+
+	}
+}
+if( isset($_GET['error']) && isset($_GET['error_description']) )//if any error
+{
+	header("Location:".admin_url('admin.php?page=linkedin-auto-publish-settings&msg=1'));
+	exit();
+}
+else if(isset($_GET['code']) && isset($_GET['state']) && $_GET['state']==$state)
+{
+
+	$fields='grant_type=authorization_code&code='.$_GET['code'].'&redirect_uri='.$redirecturl.'&client_id='.$lnappikey.'&client_secret='.$lnapisecret;
+	$ln_acc_tok_json=xyzsmap_getpage('https://www.linkedin.com/uas/oauth2/accessToken', '', false, $fields);
+	$ln_acc_tok_json=$ln_acc_tok_json['content'];
+
+	update_option('xyz_lnap_application_lnarray', $ln_acc_tok_json);
+	update_option('xyz_lnap_lnaf',0);
+	
+	header("Location:".admin_url('admin.php?page=linkedin-auto-publish-settings&msg=4'));
+	exit();
+}
+
+
+
+
+
+
+
+?>
